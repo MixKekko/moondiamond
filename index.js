0cf6cdca3908ca110087e29294de29af1ef21cd0
edodapp <gamingdodo07@gmail.com>
2022-12-08 15:11:08 +0100

    .

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js")
-const client = new Discord.Client()
+const client = new Discord.Client(
+    { intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES", "GUILD_BANS", "GUILD_INTEGRATIONS"] })
 
 client.login("MTA1MDM5MzAwNTMzNDQ2MjUwNA.GU3Vx9.PzHJyGJRKMyrN7EfJwr5bUH7x5C_IfxE_VxODg")
 intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES", "GUILD_BANS"] 
